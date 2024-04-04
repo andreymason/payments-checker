853fdb66ae1717d176ba31d5a0fc89816d04cd02
root <root@srv39318473.ultasrv.net>
2024-04-04 18:56:52 +0000

    fixed payid check

--- payments.py
+++ payments.py
@@ -89,20 +89,8 @@ perfectmoney_api = HttpPayment("https://perfectmoney.com/api/")
 sticpay = HttpPayment("https://api.sticpay.com/v2/utilities/geoip")
 muchbetter = HttpPayment("https://w.api.muchbetter.com/merchant/finish")
 pay4fun = HttpPayment("https://p4f.com/")
+payid = HttpPayment("https://payid.com.au/")
 
-print(jeton.check_accessibility())
-print("")
-print(perfectmoney_api.check_accessibility())
-print("")
-print(sticpay.check_accessibility())
-print("")
-print(muchbetter.check_accessibility())
-print("")
-print(pay4fun.check_accessibility())
-print("")
-
-
-payid = SaasHub("payid")
 n26 = SaasHub("n26")
 paysafe = SaasHub("paysafe")
 qiwi = SaasHub("qiwi")
@@ -112,28 +100,6 @@ neteller = SaasHub("neteller")
 skrill = SaasHub("skrill")
 bitpay = SaasHub("bitpay")
 coinspaid = SaasHub("coinspaid")
-
-print(payid.check_accessibility())
-print("")
-print(n26.check_accessibility())
-print("")
-print(paysafe.check_accessibility())
-print("")
-print(qiwi.check_accessibility())
-print("")
-print(paysafecard.check_accessibility())
-print("")
-print(perfectmoney.check_accessibility())
-print("")
-print(neteller.check_accessibility())
-print("")
-print(skrill.check_accessibility())
-print("")
-print(bitpay.check_accessibility())
-print("")
-print(coinspaid.check_accessibility())
-print("")
-
 visa = DownDetector("visa")
 mastercard = DownDetector("mastercard")
 apple_pay = DownDetector("apple-pay")
@@ -144,21 +110,40 @@ revolut = DownDetector("revolut")
 coinbase = DownDetector("coinbase")
 binance = DownDetector("binance")
 
-print(visa.check_accessibility())
-print("")
-print(mastercard.check_accessibility())
-print("")
-print(apple_pay.check_accessibility())
-print("")
-print(paypal.check_accessibility())
-print("")
-print(trustly.check_accessibility())
-print("")
-print(klarna.check_accessibility())
-print("")
-print(revolut.check_accessibility())
-print("")
-print(coinbase.check_accessibility())
-print("")
-print(binance.check_accessibility())
+# Формирование строки оповещения
+notification_message = ""
 
+# Проверка каждого объекта и формирование оповещения при необходимости
+for name, obj in objects.items():
+    print(name)
+    response = obj.check_accessibility()
+    print (obj)
+    
+    # Обработка объектов типа DownDetector
+    if isinstance(obj, DownDetector):
+        for country, status in response.items():
+            if status == "danger":
+                notification_message += f"Платежный сервис {name} недоступен в {country.upper()}\n"
+    
+    # Обработка объектов типа SaasHub
+    elif isinstance(obj, SaasHub):
+        if "Down" in response.values():
+            notification_message += f"Платежный сервис {name} недоступен\n"
+    
+    # Обработка ссылочных объектов
+    else:
+        for country, status_code in response.items():
+            expected_code = 200
+
+            if obj.url == "https://api.sticpay.com/v2/utilities/geoip":
+                expected_code = 401
+
+            if obj.url == "https://www.jeton.com/payv3/login":
+                expected_code = 301
+
+            if status_code != expected_code:
+                notification_message += f"Платежный сервис {name} недоступен\n"
+
+# Отправка оповещения, если есть что отправлять
+if notification_message:
+    send_to_slack(notification_message)
